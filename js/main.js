e306e7a53b7436eb0db6bfcc1f4076a35c63f644
Mavlon4658 <mavlonbe4658>
2024-12-12 21:53:13 +0500

    sign-account page is ready

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ const bodyVisible = () => {
     document.querySelector('body').style.overflow = 'visible';
 }
 
-const pModals = ['.policy-modal'];
+const pModals = ['.policy-modal', '.terms-service', '.reset-password'];
 
 pModals.forEach(cls => {
     const m = document.querySelector(cls);
@@ -36,3 +36,66 @@ pModals.forEach(cls => {
         mBg.onclick = () => closeModal();
     }
 })
+
+const alert = {
+    passwordChange: () => {
+        let el = document.querySelector('.password-change__alert');
+        if (el && !el.classList.contains('active')) {
+            el.classList.add('active');
+            setTimeout(() => {
+                el.classList.remove('active');
+                el.classList.add('end-active');
+                setTimeout(() => {
+                    el.classList.remove('end-active');
+                }, 400);
+            }, 3000);
+        }
+    },
+    passwordCopy: () => {
+        let el = document.querySelector('.password-copy__alert');
+        if (el && !el.classList.contains('active')) {
+            el.classList.add('active');
+            setTimeout(() => {
+                el.classList.remove('active');
+                el.classList.add('end-active');
+                setTimeout(() => {
+                    el.classList.remove('end-active');
+                }, 400);
+            }, 3000);
+        }
+    }
+};
+
+const resetPasswordBtn = document.querySelector('.reset-passowrd__btn');
+const passwordGenerate = document.querySelector('.password-generate');
+const copyPasswordBtn = document.querySelector('.password-copy__btn');
+const backLogin = document.querySelector('.back-login');
+
+if (resetPasswordBtn) {
+    resetPasswordBtn.onclick = e => {
+        e.preventDefault();
+        alert.passwordChange();
+        passwordGenerate.classList.add('active');
+    }
+}
+
+if (copyPasswordBtn) {
+    copyPasswordBtn.onclick = e => {
+        e.preventDefault();
+        alert.passwordCopy();
+        const temp = document.createElement("input")
+        temp.type = "text"
+        temp.value = copyPasswordBtn.querySelector('span').textContent;
+
+        document.body.appendChild(temp)
+        temp.select()
+        document.execCommand("Copy")
+        document.body.removeChild(temp)
+    }
+}
+
+if (backLogin) {
+    backLogin.onclick = () => {
+        document.querySelector('.reset-password .p-modal__close').onclick();
+    }
+}
